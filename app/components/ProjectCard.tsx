e6ca2a090d69ee653e6382340e0c48a792482a75
agup354 <140978395+agup354@users.noreply.github.com>
2026-09-02 18:53:32 +0200

    feat: improve ProjectCard visual hierarchy

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -17,8 +17,8 @@ export default function ProjectCard({
     <Link
       href={href}
       className="
-      group
-    border
+    group
+    border-slate-300
     rounded-xl
     p-6
     shadow
@@ -30,10 +30,11 @@ export default function ProjectCard({
     >
       <span className="inline-block text-4xl mb-4">{emoji}</span>
 
-      <h2>{titulo}</h2>
+      <h2 className="text-xl text-blue-700 font-bold">{titulo}</h2>
 
-      <p>{descripcion}</p>
-      <p className="mt-5  text-sm text-gray-500 transition group-hover:translate-x-1">
+      <p className="text-gray-600">{descripcion}</p>
+
+      <p className="mt-5 text-sm text-blue-600 transition group-hover:translate-x-1">
         Ver proyecto →
       </p>
     </Link>
